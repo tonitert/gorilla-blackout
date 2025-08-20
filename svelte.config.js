3728e58ed81503f565d79c976cfbbc691bedfdd4
Toni Tertsonen <toni@tertsonen.xyz>
2025-08-21 00:13:35 +0300

    fix: fixes for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,18 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.includes('dev') ? '' : '/gorilla-blackout'
+		}
+	}
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
 			"tonipc.kitty-celsius.ts.net"
 		]
 	},
+	
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
