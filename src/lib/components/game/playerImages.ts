f139f6cea1c5cc50e6ee47c731e2f01e3310e3bb
Toni Tertsonen <toni@tertsonen.xyz>
2025-08-22 03:09:32 +0300

    WIP characters

---
+++ src/lib/components/game/playerImages.ts
@@ -0,0 +1,43 @@
+
+
+// Alcoholic drinks
+import Kalja from "$lib/assets/players/PelaajaKalja.png";
+import Lonkero from "$lib/assets/players/PelaajaLonkero.png";
+import Gambina from "$lib/assets/players/PelaajaGambina.png";
+import Punkku from "$lib/assets/players/PelaajaPunkku.png";
+import Viina from "$lib/assets/players/PelaajaViina.png";
+import Shot from "$lib/assets/players/PelaajaShot.png";
+import Desi from "$lib/assets/players/PelaajaDesi.png";
+
+// Non-alcoholic drinks
+import Redbull from "$lib/assets/players/PelaajaRedbull.png";
+import Oshee from "$lib/assets/players/PelaajaOshee.png";
+
+// Others
+import Garga from "$lib/assets/players/PelaajaGarga.png";
+import Teekkari from "$lib/assets/players/PelaajaTeekkari.png";
+import Liekkipipo from "$lib/assets/players/PelaajaLiekkipipo.png";
+import Kurkkumopo from "$lib/assets/players/PelaajaKurkkumopo.png";
+import Kissa from "$lib/assets/players/PelaajaKissa.png";
+import Nuuska from "$lib/assets/players/PelaajaNuuska.png";
+
+export const playerImages = {
+	// Alcoholic drinks
+	Kalja,
+	Lonkero,
+	Gambina,
+	Punkku,
+	Viina,
+	Shot,
+	Desi,
+	// Non-alcoholic drinks
+	Redbull,
+	Oshee,
+	// Others
+	Garga,
+	Teekkari,
+	Liekkipipo,
+	Kurkkumopo,
+	Kissa,
+	Nuuska
+};
